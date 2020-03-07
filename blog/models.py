8beaa48b6cdc29b7505000348a1a1bc4f1d8b4c4
Ashan Shanaka <jpashanshanaka@gmail.com>
2020-03-07 12:58:17 +0530

    home page project fixed

--- blog/models.py
+++ blog/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class Blog(models.Model):
     title = models.CharField(max_length=200)
-    description = models.TextField(max_length=400)
+    description = models.TextField(max_length=1000)
     date = models.DateField()
